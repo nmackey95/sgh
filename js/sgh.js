1a0986eff1df8c13b5344e63e1d925b69718eff2
nmackey95 <nmackey95@gmail.com>
2014-02-22 12:44:37 -0600

    Revert "circles"
    This reverts commit 68c2777b5c878cf4000998a45b61d74bc8fd29bd.

--- js/sgh.js
+++ js/sgh.js
@@ -1,148 +1,177 @@
 "use strict";
 
-var sliders=[
+var circles = [{
+  label: 'Gigem',
+  score: 80
+}, {
+  label: 'Aggies',
+  score: 70
+}, {
+  label: 'Howdy',
+  score: 62
+}, {
+  label: 'Dammit',
+  score: 55
+}, ]
 
-  { cx: function () {
-          return window.innerWidth/2
-        },
-    cy: function () {
-          return 50
-        },
-    r:50,
-    fill:'blue',
-    constraint: function (){}
-  },
+
+$("#data_switch").on("pagecreate", function() {
+
+  var svg = d3.select('#canvas')
+    .append('svg')
+    .attr('width', window.innerWidth)
+    .attr('height', window.innerHeight)
+
+
+  var interval = 2 * Math.PI / circles.length
+
+  var radius = 100;
+
+  var color = d3.scale.category10();
+
+  var arc2deg = function(x) {
+    return x != 0 ? 180 * (x / Math.PI) : 0
+  }
+
+
+  var transformation = function(options) {
+    this.options = options;
+    this.text = function() {
+      options = this.options;
+      return 'translate(' + options.translate.x + ',' + options.translate.y + ') scale(' + options.scale.x + ',' + options.scale.y + ') rotate(' + options.rotate + ')'
+    }
+  }
+
+  var wheel_options = {
+    translate: {
+      x: window.innerWidth /2 ,
+      y: window.innerHeight* .75
+    },
+    scale: {
+      x: 3,
+      y: 2
+    },
+    rotate: 0
+  }
+
+
+  var score_options = {
+    translate: {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 4
+    },
+    scale: {
+      x: 5,
+      y: 5
+    },
+    rotate: 0
+  }
+
+  var scores = d3.scale.quantize()
+    .domain([0,100])
+    .range(['F','F','F','F','F','D','C','B','A']);
   
-  { cx: function () {
-          return window.innerWidth/2
-        },
-    cy: function () {
-          return 50
-        },
-    r:50,
-    fill:'red',
-    drag: function (){}
-  }]
+  var wheel_position = d3.scale.quantize()
+  .domain([360,0])
+  .range(circles)
+  
+ 
+  var score = svg.append('g')
+    .attr('id', 'score')
+    .append('text')
+    .text(scores(circles[0].score))
 
 
-var circles =[  
-{label:'A'},
-{label:'B'},
-
-  {label:'A'},
-  {label:'B'},
-  {label:'C'},
-  {label:'D'},
-  {label:'E'}
-]
-
-    
-    
-    function resizeAll() {
-      d3.select(this[0][0])
-        .attr('width', window.innerWidth)
-        .attr('height', window.innerHeight)
-    }
-
-    var drag = d3.behavior.drag()
-        .origin(Object)
-        .on("drag", dragmove);
-        
-    function dragmove(d) {
-      var this_circle = d3.select(this);
-      this_circle
-        .attr("cy",(+this_circle.attr('cy'))+d3.event.dy) 
-        .attr("cx",(+this_circle.attr('cx'))+d3.event.dx) 
-
-    }
+  var score_trans = new transformation(score_options)
 
 
-    $("#data_switch").on("pagecreate", function() {
+  score.attr('transform',score_trans.text())
+
+  var wheel_trans = new transformation(wheel_options)
 
 
-        var svg = d3.select('#canvas')
-          .append('svg')
-            .attr('width',window.innerWidth)
-            .attr('height',window.innerHeight/2)
+  var circle_drag = function(d) {
 
-        // svg.selectAll('circles')
-        //   .data(sliders)
-        //   .enter() 
-        //   .append('circle')
-        //   .attr('cx', function (d) {
-        //     return d.cx()
-        //   })
-        //   .attr('cy', function (d) {
-        //     return d.cy()
-        //   })
-        //   .attr('r', function (d) {
-        //     return d.r
-        //   })
-        //   .attr('fill',function (d) {
-        //     return d.fill
-        //   })
-        //   .call(drag)
+    //shift for translate
+    d3.event.x -= wheel_options.translate.x
+    d3.event.y -= wheel_options.translate.y
 
-      var maxR = Math.PI*2
+
+    // //get angle from g center to mouse         
+
+    var start_angle = Math.atan2(d3.event.y - d3.event.dy, d3.event.x - d3.event.dx);
+    var end_angle = Math.atan2(d3.event.y, d3.event.x);
+    var radian_diff = end_angle - start_angle;
+    var degree_diff = Math.round(arc2deg(radian_diff))
+
+
+    // if (degree_diff != 0) {
           
-      var interval = maxR/circles.length
-      
-      var radius = 100;
-
-      var g = svg.append('g')
-      
-      var color =  d3.scale.category10();
-      
-      var arc2deg = function (x) {
-        return Math.round((x > 0 ? x : (maxR + x)) * 360 / maxR)
-      }
-      
-      
-      var circle_drag =  function(d) {
+      var new_rotate =  wheel_options.rotate + degree_diff;
         
-        circle_drag.rotation= circle_drag.rotation || 0;
-
-            // //get angle from g center to mouse         
-            var mouse_angle = Math.atan2(150-d3.event.y, 150-d3.event.x);
-            
-            var el = d3.select(this),
-            elx = el.attr('cx'),
-            ely = el.attr('cy');
-            var element_angle = Math.atan2(150-ely, 150-elx);
-
-            circle_drag.rotation = circle_drag.rotation + (mouse_angle - element_angle)
-            
-            g.attr('transform','translate(150,150) rotate('+arc2deg(circle_drag.rotation)+')')
-            
-          };
+      wheel_options.rotate = new_rotate >= 0 ? new_rotate%360 : 360-(new_rotate%360)
       
-
-
-
-      var rotate = d3.behavior.drag()
-          // .origin( function (d) {
-          //   return {x:150,y:150}; 
-          // })
-          .on("drag", circle_drag);
+      wheel.attr('transform', wheel_trans.text())
+  
+  
+      var category = wheel_position(wheel_options.rotate)
+      d3.select('#score').select('text') .text(scores(category.score))
       
-      g.selectAll('circles')
-        .data(circles)
-        .enter() 
-        .append('circle')
-        .attr({
-          cy:function(d,i){return radius*Math.sin(i*interval);},
-          cx:function(d,i){return radius*Math.cos(i*interval);},
-          r:20,
-          fill:function(d,i){return color(i);}
-        })
-        .call(rotate)
-    
-    
-    g.attr('transform','translate(150,150)');
+      
+    // }
 
-    // g.attr({x:150,y:150})
+  };
+
+  var rotate = d3.behavior.drag()
+    .on("drag", circle_drag);
+
+  var wheel = svg.append('g')
+    .attr('id', 'wheel')
+    .call(rotate)
 
 
+  var arc = d3.svg.arc()
+    .outerRadius(100)
+    .innerRadius(20)
+    .startAngle(function(d, i) {
+      return i * interval;
+    })
+    .endAngle(function(d, i) {
+      return (i + 1) * interval;
     });
 
-  
+  var arcs = wheel.selectAll('g.arcs')
+    .data(circles)
+    .enter()
+    .append('g')
+    .attr('class', 'arcs dont_select')
+
+
+  arcs.append("svg:path")
+    .attr({
+      id: function(d, i) {
+        return 'path' + i
+      },
+      d: arc,
+      fill: function(d, i) {
+        return color(i);
+      }
+    })
+
+
+  arcs.append('text')
+    .attr('class', 'dont_select')
+    .append("textPath")
+    .attr("xlink:href", function(d, i) {
+      return '#path' + i
+    })
+    .attr('transform', 'translate(20,20)')
+    .text(function(d) {
+      return d.label
+    })
+
+
+  wheel.attr('transform', wheel_trans.text());
+
+
+});
